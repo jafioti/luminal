b0c88bfcf4094a67fc04c0b1e613b360a71674c2
Joe Fioti <jafioti@gmail.com>
2023-07-31 09:19:17 -0500

    Update module.rs

--- src/core/module.rs
+++ src/core/module.rs
@@ -12,13 +12,3 @@ pub trait Module<I> {
     type Output;
     fn forward(&self, input: I) -> Self::Output;
 }
-
-/// An already-initilized module that can be loaded from a state dict
-pub trait LoadModule {
-    fn load(&mut self, state_dict: &mut StateDict);
-}
-
-pub struct StateDict {
-    /// Map from a tensor key to the tensor data and shape
-    pub data: HashMap<String, (Vec<f32>, Vec<usize>)>,
-}
