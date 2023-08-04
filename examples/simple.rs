b819b75dfe31081806d1d1163424558dba56903d
Joe Fioti <jafioti@gmail.com>
2023-08-03 22:09:52 -0500

    Fix

--- examples/simple.rs
+++ examples/simple.rs
@@ -1,15 +1,18 @@
-use luminal::prelude::*;
+use luminal::{nn::linear::Linear, prelude::*};
 
 fn main() {
-    let cx = Graph::new();
+    let mut cx = Graph::new();
     let model: Linear<4, 5> = InitModule::initialize(&mut cx);
     let a = cx.new_tensor::<R1<4>>();
     let b = model.forward(a);
-    
+
     a.set(vec![1., 2., 3., 4.]);
     b.mark();
 
     cx.execute();
 
-    println!("B: {:?}", b.retrieve().unwrap().real_data(b.view().unwrap()).unwrap());
+    println!(
+        "B: {:?}",
+        b.retrieve().unwrap().real_data(b.view().unwrap()).unwrap()
+    );
 }
