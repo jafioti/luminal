a772ff34840ff9a51f061a0caed6daf1abc069e6
Joe Fioti <jafioti@gmail.com>
2023-08-10 23:02:40 -0500

    Finished version 1 graph selector, untested

--- src/core/optimizer.rs
+++ src/core/optimizer.rs
@@ -1,6 +1,12 @@
-use std::sync::{Arc, Mutex};
+use std::{
+    collections::HashSet,
+    sync::{Arc, Mutex},
+};
 
-use petgraph::stable_graph::NodeIndex;
+use petgraph::{
+    stable_graph::{NodeIndex, StableGraph},
+    visit::EdgeRef,
+};
 
 use crate::{graph::Graph, op::Operator, prelude::RealDim};
 
@@ -48,23 +54,131 @@ tuple_impls!(
 pub struct GraphSelector {
     #[allow(clippy::type_complexity)]
     graph: Arc<
-        Mutex<
-            petgraph::Graph<
-                (
-                    Box<dyn Operator>,
-                    Option<Vec<RealDim>>,
-                    Option<*mut NodeIndex>,
-                ),
-                (),
-            >,
-        >,
+        Mutex<petgraph::Graph<(Box<dyn Operator>, Option<Vec<RealDim>>, Vec<*mut NodeIndex>), ()>>,
     >,
+}
+
+pub struct GraphSearch<'a> {
+    selector: GraphSelector,
+    graph: &'a Graph,
+}
+
+impl<'a> Iterator for GraphSearch<'a> {
+    type Item = ();
+
+    fn next(&mut self) -> Option<Self::Item> {
+        // Look through graph for pattern from selector
+        let Some(select_start) = self.selector.graph.lock().unwrap().node_indices().next() else {
+            return None;
+        };
+        for node in self.graph.graph.node_indices() {
+            if search(
+                select_start,
+                &self.selector.graph.lock().unwrap(),
+                node,
+                &self.graph.graph,
+                NodeIndex::default(),
+            ) {
+                return Some(());
+            }
+        }
+        None
+    }
+}
+
+#[allow(clippy::type_complexity)]
+fn search(
+    selector_node: NodeIndex,
+    selector_graph: &petgraph::Graph<
+        (Box<dyn Operator>, Option<Vec<RealDim>>, Vec<*mut NodeIndex>),
+        (),
+    >,
+    graph_node: petgraph::stable_graph::NodeIndex,
+    graph: &StableGraph<(Box<dyn Operator>, Vec<RealDim>), u8>,
+    coming_from: NodeIndex,
+) -> bool {
+    let selector_weight = selector_graph.node_weight(selector_node).unwrap();
+    let current_weight = graph.node_weight(graph_node).unwrap();
+    if current_weight.0.as_any().type_id() != selector_weight.0.as_any().type_id() {
+        return false;
+    }
+    if let Some(shape) = &selector_weight.1 {
+        if shape.len() != current_weight.1.len() {
+            return false;
+        }
+        for (a, b) in shape.iter().zip(current_weight.1.iter()) {
+            if a != b {
+                return false;
+            }
+        }
+    }
+    // Match outgoing
+    let mut used = HashSet::new();
+    for select_outgoing in
+        selector_graph.edges_directed(selector_node, petgraph::Direction::Outgoing)
+    {
+        if select_outgoing.target() == coming_from {
+            continue;
+        }
+        if let Some(target) = graph
+            .edges_directed(graph_node, petgraph::Direction::Outgoing)
+            .map(|e| e.target())
+            .filter(|i| !used.contains(i))
+            .find(|i| {
+                search(
+                    select_outgoing.target(),
+                    selector_graph,
+                    *i,
+                    graph,
+                    selector_node,
+                )
+            })
+        {
+            used.insert(target);
+        } else {
+            return false;
+        }
+    }
+    // Match incoming
+    let mut used = HashSet::new();
+    for select_incoming in
+        selector_graph.edges_directed(selector_node, petgraph::Direction::Incoming)
+    {
+        if select_incoming.source() == coming_from {
+            continue;
+        }
+        if let Some(target) = graph
+            .edges_directed(graph_node, petgraph::Direction::Outgoing)
+            .map(|e| e.target())
+            .filter(|i| !used.contains(i))
+            .find(|i| {
+                search(
+                    select_incoming.source(),
+                    selector_graph,
+                    *i,
+                    graph,
+                    selector_node,
+                )
+            })
+        {
+            used.insert(target);
+        } else {
+            return false;
+        }
+    }
+    // All checks out
+    for ptr in &selector_weight.2 {
+        unsafe {
+            **ptr = graph_node;
+        }
+    }
+    true
 }
 
 impl GraphSelector {
     pub fn op<O: Operator + 'static>(&self, op: O) -> OpSelector {
         let mut m_self = self.graph.lock().unwrap();
-        let id = m_self.add_node((Box::new(op), None, None));
+        let id = m_self.add_node((Box::new(op), None, vec![]));
         OpSelector { graph: self, id }
     }
 
@@ -72,6 +186,13 @@ impl GraphSelector {
         let mut m_self = self.graph.lock().unwrap();
         m_self.add_edge(o1.id, o2.id, ());
         o2
+    }
+
+    pub fn search(self, graph: &Graph) -> GraphSearch {
+        GraphSearch {
+            selector: self,
+            graph,
+        }
     }
 }
 
@@ -92,7 +213,7 @@ impl OpSelector {
     pub fn ptr(self, ptr: *mut NodeIndex) -> Self {
         let graph = unsafe { self.graph.as_ref().unwrap() };
         let mut m_graph = graph.graph.lock().unwrap();
-        m_graph.node_weight_mut(self.id).unwrap().2 = Some(ptr);
+        m_graph.node_weight_mut(self.id).unwrap().2.push(ptr);
         self
     }
 }
@@ -101,12 +222,16 @@ impl OpSelector {
 mod tests {
     use petgraph::adj::NodeIndex;
 
-    use crate::op::{Exp2, Expand, Log2, Mul, Permute, SumReduce};
+    use crate::{
+        op::{Exp2, Expand, Log2, Mul, Permute, SumReduce},
+        prelude::Graph,
+    };
 
     use super::GraphSelector;
 
     #[test]
     fn test_graph_selector() {
+        let cx = Graph::default();
         // Exp -> Log or Log -> Exp
         let (mut exp, mut log) = (NodeIndex::default(), NodeIndex::default());
         let selector1 = GraphSelector::default();
@@ -119,6 +244,12 @@ mod tests {
             selector2.op(Exp2).ptr(&mut exp),
             selector2.op(Log2).ptr(&mut log),
         );
+
+        assert_eq!(
+            selector1.search(&cx).chain(selector2.search(&cx)).next(),
+            None
+        );
+
         // Matmul
         let s = GraphSelector::default();
         s.edge(
@@ -131,5 +262,7 @@ mod tests {
             ),
             s.op(SumReduce::default()),
         );
+
+        assert_eq!(s.search(&cx).next(), None);
     }
 }
