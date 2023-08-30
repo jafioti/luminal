cc1d92e62fa32a742c6b1a29eac4369d1105ab35
Joe Fioti <jafioti@gmail.com>
2023-08-29 21:53:49 -0500

    Remvoed function from mean reduce

--- src/hl_ops/reduction.rs
+++ src/hl_ops/reduction.rs
@@ -47,6 +47,23 @@ impl<S: Shape> GraphTensor<S> {
         let mut shape = <S as Shape>::realized_shape();
         let mut node_id = self.id;
         for dim in Ax::as_array().into_iter().collect_vec().into_iter().rev() {
+            // Create div tensor
+            // Create ones tensor and expand up to full tensor shape
+            let mut ones = graph.constant(1.0).id;
+            let mut ones_shape = vec![];
+            for (dim, size) in shape.iter().enumerate() {
+                ones_shape.insert(dim, *size);
+                ones = graph
+                    .add_op(op::Expand(dim, *size), ones_shape.clone())
+                    .input(ones)
+                    .finish();
+            }
+            ones_shape.remove(dim as usize);
+            // Sum reduce on current dimension
+            let div_tensor = graph
+                .add_op(op::SumReduce(dim as usize), ones_shape)
+                .input(ones)
+                .finish();
             // Reduce shape
             shape.remove(dim as usize);
             // Sum reduce
@@ -54,44 +71,16 @@ impl<S: Shape> GraphTensor<S> {
                 .add_op(op::SumReduce(dim as usize), shape.clone())
                 .input(node_id)
                 .finish();
-            // Create div tensor
-            let size_t = graph
-                .add_op(
-                    op::Function(
-                        "MeanDivTerm".to_string(),
-                        Box::new(move |inp, i| {
-                            let s = inp[0].1.shape.shape()[dim as usize];
-                            (
-                                Some(Tensor {
-                                    data: Box::new(vec![1. / s as f32]),
-                                }),
-                                TensorView {
-                                    tensor_id: i,
-                                    shape: ShapeTracker::new(vec![]),
-                                },
-                            )
-                        }),
-                    ),
-                    vec![],
-                )
-                .input(self.id)
-                .finish();
-            let size_t: GraphTensor<R0> = GraphTensor::from_id(size_t, graph);
-            let mut size_t = size_t.id;
-            let mut size_t_shape = vec![];
-            // Expand div tensor
-            for (dim, size) in shape.iter().enumerate() {
-                size_t_shape.insert(dim, *size);
-                size_t = graph
-                    .add_op(op::Expand(dim, *size), size_t_shape.clone())
-                    .input(size_t)
-                    .finish();
-            }
+
             // Divide by div tensor
+            let mul_tensor = graph
+                .add_op(op::Recip, shape.clone())
+                .input(div_tensor)
+                .finish();
             node_id = graph
                 .add_op(op::Mul, shape.clone())
                 .input(node_id)
-                .input(size_t)
+                .input(mul_tensor)
                 .finish();
         }
         GraphTensor::from_id(node_id, self.graph_ref)
