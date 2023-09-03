eb1355c65a961f38391fd49d43e615dbf376a47d
Joe Fioti <jafioti@gmail.com>
2023-09-03 18:46:56 -0500

    More fixes

--- src/hl_ops/reduction.rs
+++ src/hl_ops/reduction.rs
@@ -50,22 +50,15 @@ impl<S: Shape> GraphTensor<S> {
         for dim in Ax::as_array().into_iter().collect_vec().into_iter().rev() {
             // Create div tensor
             // Create ones tensor and expand up to full tensor shape
-            let mut ones = self.graph().constant(1.0).id;
-            let mut ones_shape =
-                crate::core::shape::simple_tracker::ShapeTracker::new(&shape.shape());
-            ones = self
-                .graph()
-                .add_op(op::NoOp)
-                .input(ones, ones_shape)
-                .input(node_id, shape)
-                .finish();
+            let ones = self.graph().constant(1.0).id;
             // Sum reduce on current dimension
+            let mut st = ShapeTracker::new(&[]);
+            st.expand(0, shape.shape()[dim as usize]);
             let div_tensor = self
                 .graph()
-                .add_op(op::SumReduce(dim as usize))
-                .input(ones, ones_shape)
+                .add_op(op::SumReduce(0))
+                .input(ones, st)
                 .finish();
-            ones_shape.remove_dim(dim as usize);
             // Sum reduce
             node_id = self
                 .graph()
@@ -78,13 +71,13 @@ impl<S: Shape> GraphTensor<S> {
             let mul_tensor = self
                 .graph()
                 .add_op(op::Recip)
-                .input(div_tensor, shape)
+                .input(div_tensor, ShapeTracker::new(&[]))
                 .finish();
             node_id = self
                 .graph()
                 .add_op(op::Mul)
                 .input(node_id, shape)
-                .input(mul_tensor, shape)
+                .input(mul_tensor, ShapeTracker::fake(&shape.shape()))
                 .finish();
         }
         GraphTensor::from_id(node_id, shape, self.graph_ref)
@@ -95,6 +88,40 @@ impl<S: Shape> GraphTensor<S> {
 mod tests {
     use crate::{prelude::*, tests::assert_close_data};
     use dfdx::prelude::*;
+
+    #[test]
+    fn test_sum_reduce() {
+        let mut cx = Graph::new();
+        let a = cx.new_tensor::<R2<2, 3>>("Input");
+        a.set(vec![1., 2., 3., 1., 2., 3.]);
+        let b = a.sum_reduce::<_, crate::prelude::Axis<1>>();
+        b.mark();
+
+        cx.execute();
+
+        let d_dev = Cpu::default();
+        let d_a = d_dev.tensor([[1., 2., 3.], [1., 2., 3.]]);
+        let d_b = d_a.sum::<_, dfdx::shapes::Axis<1>>();
+
+        assert_close_data(&b.data(), &d_b.as_vec());
+    }
+
+    #[test]
+    fn test_max_reduce() {
+        let mut cx = Graph::new();
+        let a = cx.new_tensor::<R2<2, 3>>("Input");
+        a.set(vec![1., 2., 3., 1., 2., 3.]);
+        let b = a.max_reduce::<_, crate::prelude::Axis<1>>();
+        b.mark();
+
+        cx.execute();
+
+        let d_dev = Cpu::default();
+        let d_a = d_dev.tensor([[1., 2., 3.], [1., 2., 3.]]);
+        let d_b = d_a.max::<_, dfdx::shapes::Axis<1>>();
+
+        assert_close_data(&b.data(), &d_b.as_vec());
+    }
 
     #[test]
     fn test_mean_reduce() {
