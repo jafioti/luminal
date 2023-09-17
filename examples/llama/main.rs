41d88b0c4abf6f005da6f629a3afad27e38a7d05
Joe Fioti <jafioti@Joes-MacBook-Pro.local>
2023-09-17 11:30:16 -0500

    Multi-graph llama

--- examples/llama/main.rs
+++ examples/llama/main.rs
@@ -20,9 +20,10 @@ fn main() {
     ).token_ids.iter() .map(|&x| x as usize).collect();
     input.insert(0, 1);
 
-    println!("Creating Graph...");
-    let mut cx = Graph::new();
-    let model: LlamaForCausalLM<
+    println!("Creating Graphs...");
+    let mut cx1 = Graph::new();
+    let mut cx2 = Graph::new();
+    let mut model: LlamaForCausalLM<
         { config::VOCAB },
         { config::HEADS },
         { config::HIDDEN },
@@ -30,56 +31,58 @@ fn main() {
         { config::HEAD_DIM },
         { config::HEAD_DIM_OVER_2 },
         { config::LAYERS },
-    > = InitModule::initialize(&mut cx);
-    let mut inp = cx.new_tensor::<(Dyn<'b'>, Dyn<'s'>)>("Input");
-    let (out, cache_src) = model.forward(inp);
-    let cache_src = cache_src.into_iter().map(|(a, b)| {
-        (a.reshape(), b.reshape())
-    }).collect::<Vec<_>>();
-    out.mark();
-    for (k, v) in &cache_src {
+    > = InitModule::initialize(&mut cx1);
+    let mut inp = cx1.new_tensor::<(Dyn<'b'>, Dyn<'s'>)>("Input");
+    let (out1, cache1) = model.forward(inp);
+    out1.mark();
+    for (k, v) in &cache1 {
         k.mark_no_delete();
         v.mark_no_delete();
     }
-
-    println!("Loading...");
-    loader::DfdxDeferredLoader::new("./examples/llama/setup/llama-7b-hf").load(&model, &mut cx);
-
-    println!("Inferencing...");
-    // First pass
-    inp.set_dyn(input.clone(), vec![1, input.len()]);
-    let now = std::time::Instant::now();
-
-    cx.optimize(CPUOptimizer::default());
-    cx.execute();
-    println!("Forward Pass Took {:.2}s", now.elapsed().as_secs_f32());
-
-    let out = out.dyn_data(&cx.dyn_map);
-    input.push(sample_index(&out[out.len() - 32_000..]));
-    println!("{}", tokenizer.decode(&input.iter().map(|i| *i as i64).collect::<Vec<_>>(), true, false));
-
+    loader::DfdxDeferredLoader::new("./examples/llama/setup/llama-7b-hf").load(&model, &mut cx1);
+    cx1.optimize(<(CPUOptimizer, GenericOptimizer)>::default());
 
     // Build KV cache forward graph
-    let mut single_inp = cx.new_tensor::<(Dyn<'b'>, Const<1>)>("Single Input");
-    let (out, cache_dest)= model.forward_kv::<_, _, Dyn<'p'>, Dyn<'t'>>((single_inp, cache_src.clone()));
+    model = InitModule::initialize(&mut cx2);
+    let mut single_inp = cx2.new_tensor::<(Dyn<'b'>, Const<1>)>("Input");
+    let cache_src = (0..config::LAYERS).map(|_| (cx2.new_tensor("Key Cache"), cx2.new_tensor("Value Cache"))).collect::<Vec<_>>();
+    let (out, cache_dest)= model.forward_kv::<_, _, Dyn<'s'>, Dyn<'t'>>((single_inp, cache_src.clone()));
     out.mark();
     for (k, v) in &cache_dest {
         k.mark_no_delete();
         v.mark_no_delete();
     }
-    cx.prune([out.id], cache_src.iter().flat_map(|(k, v)| [k.id, v.id]));
-    cx.optimize(<(CPUOptimizer, GenericOptimizer)>::default());
+    loader::DfdxDeferredLoader::new("./examples/llama/setup/llama-7b-hf").load(&model, &mut cx2);
+    cx2.optimize(<(CPUOptimizer, GenericOptimizer)>::default());
+
+    println!("Inferencing...");
+    // First pass
+    inp.set_dyn(input.clone(), vec![1, input.len()]);
+    let now = std::time::Instant::now();
+    cx1.execute();
+    println!("Forward Pass Took {:.2}s", now.elapsed().as_secs_f32());
+
+    let out1 = out1.dyn_data(&cx1.dyn_map);
+    input.push(sample_index(&out1[out1.len() - 32_000..]));
+    println!("{}", tokenizer.decode(&input.iter().map(|i| *i as i64).collect::<Vec<_>>(), true, false));
+
+    // Move cache over to second graph
+    for ((key_src, val_src), (key_dest, val_dest)) in cache1.into_iter().zip(cache_src.iter()) {
+        cx2.tensors.insert(key_dest.id, cx1.tensors.remove(&key_src.id).unwrap());
+        cx2.tensors.insert(val_dest.id, cx1.tensors.remove(&val_src.id).unwrap());
+    }
+    drop(cx1);
 
     loop {
         single_inp.set_dyn(vec![*input.last().unwrap()], vec![1, 1]);
-        cx.set_dyn_dim('p', input.len() - 1);
-        cx.set_dyn_dim('t', input.len());
+        cx2.set_dyn_dim('s', input.len() - 1);
+        cx2.set_dyn_dim('t', input.len());
 
         let now = std::time::Instant::now();
-        cx.execute();
+        cx2.execute();
         println!("Forward Pass Took {:.2}s", now.elapsed().as_secs_f32());
         
-        let o = out.dyn_data(&cx.dyn_map);
+        let o = out.dyn_data(&cx2.dyn_map);
         out.drop();
         // Sample tokens
         input.push(sample_index(&o));
@@ -88,8 +91,8 @@ fn main() {
         // Swap caches
         for ((src_k, src_v), (dest_k, dest_v)) in cache_src.iter().copied().zip(cache_dest.iter().copied()) {
             // Move dest caches to src
-            cx.swap_tensors(src_k, dest_k);
-            cx.swap_tensors(src_v, dest_v);
+            cx2.swap_tensors(src_k, dest_k);
+            cx2.swap_tensors(src_v, dest_v);
             // Drop dest caches
             dest_k.drop();
             dest_v.drop();
