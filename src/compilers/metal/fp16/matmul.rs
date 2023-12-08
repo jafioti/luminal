b87b30f0450000c1531e37b68331107907c3e3f8
Joe Fioti <jafioti@Joes-MacBook-Pro.local>
2023-12-08 12:01:54 -0600

    simd batch matmul

--- src/compilers/metal/fp16/matmul.rs
+++ src/compilers/metal/fp16/matmul.rs
@@ -193,54 +193,68 @@ impl Operator for MetalMatmul2D {
 pub struct MetalBatchMatmul2D(ComputePipelineState, CommandQueue, Device);
 
 impl MetalBatchMatmul2D {
-    fn compile(dev: &Device, a_row_major: bool, b_row_major: bool) -> ComputePipelineState {
+    fn compile(dev: &Device) -> ComputePipelineState {
         compile_function(
-                    "kernel_batch_matmul_2d",
-                        &format!("
-                        #include <metal_stdlib>
-                        using namespace metal;
-                        kernel void kernel_batch_matmul_2d(
-                            device half *A [[buffer(0)]],
-                            device half *B [[buffer(1)]],
-                            device half *C [[buffer(2)]],
-                            device uint& Batch [[buffer(3)]],
-                            device uint& M [[buffer(4)]],
-                            device uint& N [[buffer(5)]],
-                            device uint& K [[buffer(6)]],
-                            threadgroup half* shared_memory [[threadgroup(0)]],
-                            uint3 global_pos [[thread_position_in_grid]],
-                            uint3 local_pos [[thread_position_in_threadgroup]],
-                            uint3 block_size [[threads_per_threadgroup]]
-                        ) {{
-                            float sum = 0.0f;
-                        
-                            threadgroup half* b_start = shared_memory + block_size.x * block_size.x;
-                            uint local_x_block_x = local_pos.x * block_size.x;
-                            uint shared_mem_addr = local_x_block_x + local_pos.y;
-                            uint common_a_ind = global_pos.x * K + local_pos.y;
-                            uint common_b_ind = global_pos.y * K + local_pos.x;
-                            for (uint m = 0; m < K; m += block_size.x) {{
-                                shared_memory[shared_mem_addr] = (local_pos.y + m < K) ? A[global_pos.z * M * K + {}] : 0.0h;
-                                b_start[shared_mem_addr] = (local_pos.x + m < K) ? B[{}] : 0.0h;
-                        
-                                threadgroup_barrier(mem_flags::mem_threadgroup);
-                        
-                                #pragma unroll(8)
-                                for (uint e = 0; e < block_size.x; ++e) {{
-                                    sum = fast::fma((float)shared_memory[local_x_block_x + e], (float)b_start[e * block_size.x + local_pos.y], sum);
-                                }}
-                                threadgroup_barrier(mem_flags::mem_threadgroup);
-                            }}
-                            
-                            if (global_pos.x < M && global_pos.y < N && global_pos.z < Batch) {{
-                                C[global_pos.z * M * N + global_pos.x * N + global_pos.y] = (half)sum;
-                            }}
-                        }}", 
-                        if a_row_major {"common_a_ind + m"} else {"(local_pos.y + m) * M + global_pos.x"}, 
-                        if b_row_major {"(m + local_pos.x) * N + global_pos.y"} else {"common_b_ind + m"}
-                    ),
-                    dev,
-                )
+            "kernel_batch_matmul_2d",
+            "
+#include <metal_stdlib>
+#include <metal_simdgroup_matrix>
+#include <metal_simdgroup>
+using namespace metal;
+
+kernel void kernel_batch_matmul_2d(
+    device const half *data1 [[buffer(0)]],
+    device const half *data2 [[buffer(1)]],
+    device half *a [[buffer(2)]],
+    device uint& M [[buffer(3)]],
+    device uint& N [[buffer(4)]],
+    device uint& K [[buffer(5)]],
+    uint3 block_pos [[threadgroup_position_in_grid]],
+    uint3 global_pos [[thread_position_in_grid]]
+) {
+    a += M * N * block_pos.z + block_pos.x * 32 * N + global_pos.y * 32;
+    data1 += M * K * block_pos.z + block_pos.x * 32 * K;
+    data2 += global_pos.y * 32;
+
+    simdgroup_float8x8 acc[4][4];
+    for (uint i = 0; i < 4; ++i) {
+        for (uint j = 0; j < 4; ++j) {
+            acc[i][j] = simdgroup_float8x8(0);
+        }
+    }
+
+    simdgroup_half8x8 A[4];
+    simdgroup_half8x8 B[4];
+    uint k8 = 8 * K;
+    for (uint k = 0; k < K; k+=8) {
+        threadgroup_barrier(mem_flags::mem_threadgroup);
+        device const half *d1 = data1 + k;
+        for (int i = 0; i < 4; ++i) {
+            simdgroup_load(A[i], d1 + i * k8, K);
+            simdgroup_load(B[i], data2 + k * N + i * 8, N);
+        }
+
+        for (int i = 0; i < 4; ++i) {
+            for (int j = 0; j < 4; ++j) {
+                simdgroup_multiply_accumulate(acc[i][j], A[j], B[i], acc[i][j]);
+            }
+        }
+    }
+
+    simdgroup_half8x8 temp = simdgroup_half8x8(0);
+    simdgroup_half8x8 ident = simdgroup_half8x8(1);
+    // Width
+    for (int i = 0; i < 4; ++i) {
+        uint n8i = i * 8 * N;
+        // Height
+        for (int j = 0; j < 4; ++j) {
+            simdgroup_multiply(temp, acc[j][i], ident);
+            simdgroup_store(temp, a+(8*j+n8i), N);
+        }
+    }
+}",
+            dev,
+        )
     }
 }
 
@@ -272,21 +286,19 @@ impl MetalKernelForward for MetalBatchMatmul2D {
         encoder.set_buffer(0, Some(inputs[0].0), 0);
         encoder.set_buffer(1, Some(inputs[1].0), 0);
         encoder.set_buffer(2, Some(&out), 0);
-        encoder.set_int(3, batch_size as u32);
-        encoder.set_int(4, m as u32);
-        encoder.set_int(5, n as u32);
-        encoder.set_int(6, k as u32);
-        encoder.set_threadgroup_memory_length(0, 8 * 8 * 2 * std::mem::size_of::<f16>() as u64);
+        encoder.set_int(3, m as u32);
+        encoder.set_int(4, n as u32);
+        encoder.set_int(5, k as u32);
 
         // Execute
         encoder.dispatch_thread_groups(
             MTLSize {
-                width: (m as u64).div_ceil(8),
-                height: (n as u64).div_ceil(8),
+                width: (m as u64).div_ceil(32),
+                height: (n as u64).div_ceil(32 * 8),
                 depth: batch_size as u64,
             },
             MTLSize {
-                width: 8,
+                width: 32,
                 height: 8,
                 depth: 1,
             },
@@ -558,7 +570,7 @@ impl Compiler for MetalMatMulCompiler {
             }
             let mut matmul_op = graph
                 .add_op(MetalBatchMatmul2D(
-                    MetalBatchMatmul2D::compile(&dev, true, true),
+                    MetalBatchMatmul2D::compile(&dev),
                     queue.clone(),
                     dev.clone(),
                 ))
