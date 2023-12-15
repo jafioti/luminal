404322b4abefbe573b616dcbb9398e482bd33d5d
Hassan Hayat <hassan.hayat7@gmail.com>
2023-12-15 01:23:26 -0600

    Save code

--- src/hl_ops/movement.rs
+++ src/hl_ops/movement.rs
@@ -1,3 +1,5 @@
+use itertools::Itertools;
+
 use crate::{
     op,
     prelude::{symbolic::Expression, *},
@@ -96,26 +98,214 @@ impl<S: Shape> GraphTensor<S> {
     }
 
     // For now we assume kernel size <= stride
-    pub fn pool<Dst: Shape>(self, kernel_size: &[usize], stride: &[usize]) -> GraphTensor<Dst> {
+    pub fn pool_2d_old<Dst: Shape>(
+        mut self,
+        kernel_size: (usize, usize),
+        stride: (usize, usize),
+    ) -> GraphTensor<Dst> {
         let current_shape = self.shape.shape();
         let current_dims = current_shape.len();
 
-        let mut new_shape = current_shape.clone();
+        // let kernel_sizes: Vec<Expression> =
+        //     kernel_size.iter().map(|&k| Expression::from(k)).collect();
 
-        let kernel_sizes: Vec<Expression> =
-            kernel_size.iter().map(|&k| Expression::from(k)).collect();
+        // let sliced = self.slice((.., .., .., ..));
+        // self.shape.slice
 
-        for i in 0..kernel_sizes.len() {
-            let dim = current_dims - kernel_sizes.len() + i;
-            new_shape[dim] = new_shape[dim] / stride[i];
+        let (kx, ky) = kernel_size;
+        let (sx, sy) = stride;
+
+        let mut slice_vector: Vec<(Expression, Expression)> = Vec::new();
+        for i in 0..current_dims {
+            if i == current_dims - 2 {
+                let end_shape = current_shape[i] + ((sx - kx) / (kx * kx));
+                slice_vector.push((Expression::from(0), end_shape));
+            } else if i == current_dims - 1 {
+                let end_shape = current_shape[i] + ((sy - ky) / (ky * ky));
+                slice_vector.push((Expression::from(0), end_shape));
+            } else {
+                slice_vector.push((Expression::from(0), current_shape[i]));
+            }
         }
 
-        for i in 0..kernel_sizes.len() {
-            let insert_index = current_dims - 1 + (i * 2);
-            new_shape.insert(insert_index, Expression::from(stride[i]));
+        let slices_array = slice_vector.as_slice();
+        self.shape.slice(slices_array);
+        let mut new_shape = self.shape.shape().clone();
+
+        fn new_dim(dim: usize, k: usize, s: usize) -> usize {
+            (dim - k) / s + 1
         }
 
-        return self.dyn_reshape(new_shape);
+        // Let's see if we can extract the current dim as a number
+        let dimx: Expression = current_shape[current_dims - 2];
+        let _dimx = dimx.to_usize().unwrap();
+        // let new_dimx = _dimx / sx;
+        let new_dimx = new_dim(_dimx, kx, sx);
+
+        println!(
+            "
+        dimx: {_dimx}
+        sx: {sx}
+        new dimx: {new_dimx}
+                "
+        );
+
+        let dimy: Expression = current_shape[current_dims - 1];
+        let _dimy = dimy.to_usize().unwrap();
+        // let new_dimy = _dimy / sy;
+        let new_dimy = new_dim(_dimy, ky, sy);
+        println!(
+            "
+        dimy: {_dimy}
+        sy: {sy}
+        new dimy: {new_dimy}
+                "
+        );
+
+        // new_shape[current_dims - 2] = current_shape[current_dims - 2] / sx;
+        // new_shape[current_dims - 1] = current_shape[current_dims - 1] / sy;
+        new_shape[current_dims - 2] = Expression::from(new_dimx);
+        new_shape[current_dims - 1] = Expression::from(new_dimy);
+
+        println!("New Shape: {:?}", new_shape);
+
+        new_shape.insert(current_dims - 1, Expression::from(sx));
+        new_shape.insert(current_dims - 1 + 2, Expression::from(sy));
+
+        println!("New Shape: {:?}", new_shape);
+
+        // for i in 0..kernel_sizes.len() {
+        //     let dim = current_dims - kernel_sizes.len() + i;
+        //     new_shape[dim] = new_shape[dim] / stride[i];
+        // }
+
+        // for i in 0..kernel_sizes.len() {
+        //     let insert_index = current_dims - 1 + (i * 2);
+        //     new_shape.insert(insert_index, Expression::from(stride[i]));
+        // }
+
+        let mut slice_vector: Vec<(Expression, Expression)> = Vec::new();
+        let new_dims = new_shape.len();
+        for i in 0..new_dims {
+            if i == new_dims - 3 {
+                slice_vector.push((Expression::from(0), Expression::from(kx)));
+            } else if i == new_dims - 1 {
+                slice_vector.push((Expression::from(0), Expression::from(ky)));
+            } else {
+                slice_vector.push((Expression::from(0), new_shape[i].clone()));
+            }
+        }
+
+        let mut reshaped = self.dyn_reshape(new_shape);
+        let slices_array = slice_vector.as_slice();
+        reshaped.shape.slice(slices_array);
+
+        reshaped
+    }
+
+    pub fn pool_2d<Dst: Shape>(
+        mut self,
+        kernel_size: (usize, usize),
+        stride: (usize, usize),
+    ) -> GraphTensor<Dst> {
+        let (kx, ky) = kernel_size;
+        let (sx, sy) = stride;
+
+        if sx < kx || sy < ky {
+            panic!("Currently do not support case where kernel_size > stride")
+        }
+
+        // Get the tensor dims
+        let current_shape: Vec<usize> = self
+            .shape
+            .shape()
+            .iter()
+            .map(|&dim: &Expression| dim.to_usize().unwrap())
+            .collect_vec();
+
+        let mut new_shape: Vec<usize> = Vec::new();
+        for i in 0..current_shape.len() {
+            if i < current_shape.len() - 2 {
+                new_shape.push(current_shape[i]);
+            } else if i == current_shape.len() - 2 {
+                new_shape.push((current_shape[i] - kx) / sx + 1);
+            } else if i == current_shape.len() - 1 {
+                new_shape.push((current_shape[i] - ky) / sy + 1);
+            }
+        }
+
+        let h = current_shape[current_shape.len() - 2];
+        let w = current_shape[current_shape.len() - 1];
+
+        let n_x = h / sx;
+        let max_x = (n_x - 1) * sx + kx;
+        let px = h - max_x;
+
+        let n_y = w / sy;
+        let max_y = (n_y - 1) * sy + ky;
+        let py = w - max_y;
+
+        // let max_x = (h / sx) * (sx - 1) + kx;
+        // let max_y = (w / sy) * (sy - 1) + ky;
+
+        println!("current_shape: {:?}", current_shape);
+        println!("(kx, ky) = {:?}", kernel_size);
+        println!("(sx, sy) = {:?}", stride);
+        println!("new_shape: {:?}", new_shape);
+        println!("(h, w), {:?}", (h, w));
+        println!("(max_x, max_y): {:?}", (max_x, max_y));
+        println!("(px, py): {:?}", (px, py));
+
+        // Now, we slice the it
+
+        // self.shape.slice(&[
+        //     (Expression::from(0), Expression::from(new_shape[0])),
+        //     (Expression::from(0), Expression::from(max_x)),
+        //     (Expression::from(0), Expression::from(max_y)),
+        // ]);
+
+        // self.shape.slice(&[
+        //     self.shape.slices[0],
+        //     (Expression::from(0), Expression::from(max_x)),
+        //     (Expression::from(0), Expression::from(max_y)),
+        // ]);
+
+        // Instead of slicing, we pad
+        /*
+        let a = cx.tensor::<R2<3, 2>>();
+        a.set(vec![1.4325, 2.492428, 3.127365, 33.2834, 4.18734, 23.854]);
+        let b = a.pad::<R2<3, 4>, usize, usize>(&[(0, 0), (0, 2)]);
+        */
+
+        self.shape.pad(&[
+            self.shape.padding[0],
+            (Expression::from(0), Expression::from(px)),
+            (Expression::from(0), Expression::from(py)),
+        ]);
+
+        println!("padded_shape: {:?}", self.shape);
+
+        // println!("sliced_shape: {:?}", self.shape);
+
+        let mut new_shape_expr = self.shape.shape();
+
+        for i in 0..new_shape_expr.len() {
+            new_shape_expr[i] = Expression::from(new_shape[i]);
+        }
+
+        new_shape_expr.insert(new_shape.len() - 1, Expression::from(sx));
+        new_shape_expr.insert(new_shape.len() - 1 + 2, Expression::from(sy));
+
+        let reshaped: GraphTensor<Dst> = self.dyn_reshape(new_shape_expr);
+
+        // Update the dims directly
+        // for i in self.shape.dims {
+        //     self.shape.dims[i] = new_shape[i];
+        // }
+
+        println!("reshape: {:?}", reshaped.shape);
+
+        reshaped
     }
 
     pub fn pad<Dst: Shape, Start: Into<Expression> + Copy, End: Into<Expression> + Copy>(
@@ -152,6 +342,8 @@ mod tests {
         tensor::{Cpu, TensorFrom, TensorFromVec},
         tensor_ops::{RealizeTo, TryConcatAlong},
     };
+
+    use crate::prelude::symbolic::Expression;
 
     crate::test_imports!();
 
@@ -276,6 +468,31 @@ mod tests {
     }
 
     #[test]
+    fn test_dummy_slice() {
+        let mut cx = Graph::new();
+
+        let mut a = cx.tensor::<R3<1, 4, 4>>();
+        a.set(vec![
+            12., 20., 30., 0., 8., 12., 2., 0., 34., 70., 37., 4., 112., 100., 25., 12.,
+        ]);
+
+        // I basically want to slice the top part
+        a.shape.slice(&[
+            (Expression::from(0), Expression::from(1)),
+            (Expression::from(0), Expression::from(2)),
+            (Expression::from(0), Expression::from(4)),
+        ]);
+
+        let b: GraphTensor<_> = a.realize::<R3<1, 2, 4>>();
+
+        b.retrieve();
+
+        cx.execute();
+
+        println!("{:?}", &b.data());
+    }
+
+    #[test]
     fn test_pool() {
         // Max Pool Example
         let mut cx = Graph::new();
@@ -289,7 +506,8 @@ mod tests {
         ]);
 
         let out1 = inp1
-            .pool::<R5<5, 1, 2, 2, 2>>(&[2, 2], &[2, 2])
+            // .pool::<R5<5, 1, 2, 2, 2>>(&[2, 2], &[2, 2])
+            .pool_2d::<R5<1, 2, 2, 2, 2>>((2, 2), (2, 2))
             .max_reduce::<_, LAxes2<2, 4>>();
         out1.retrieve();
 
@@ -300,20 +518,10 @@ mod tests {
         ]);
 
         let out2 = inp2
-            .pool::<R5<1, 2, 2, 2, 2>>(&[2, 2], &[2, 2])
+            // .pool::<R5<1, 2, 2, 2, 2>>(&[2, 2], &[2, 2])
+            .pool_2d::<R5<1, 2, 2, 2, 2>>((2, 2), (2, 2))
             .max_reduce::<_, LAxes2<2, 4>>();
         out2.retrieve();
-
-        // Case 3
-        let inp3 = cx.tensor::<R3<1, 4, 4>>();
-        inp3.set(vec![
-            12., 20., 30., 0., 8., 12., 2., 0., 34., 70., 37., 4., 112., 100., 25., 12.,
-        ]);
-
-        let out3 = inp3
-            .pool::<R5<1, 2, 2, 2, 2>>(&[2, 2], &[2, 1])
-            .max_reduce::<_, LAxes2<2, 4>>();
-        out3.retrieve();
 
         // Run all the computations
         cx.execute();
@@ -324,7 +532,47 @@ mod tests {
             &[87., 48., 54., 99., 94., 85., 98., 96., 82., 70.],
         );
         assert_close(&out2.data(), &[20., 30., 112., 37.]);
-        assert_close(&out3.data(), &[20., 30., 30., 112., 100., 37.]);
+    }
+
+    #[test]
+    fn test_pool_non_square() {
+        let mut cx = Graph::new();
+        // Case 3
+        let inp3 = cx.tensor::<R3<1, 4, 4>>();
+        inp3.set(vec![
+            12., 20., 30., 0., 8., 12., 2., 0., 34., 70., 37., 4., 112., 100., 25., 12.,
+        ]);
+
+        let out3 = inp3
+            // .pool::<R5<1, 2, 2, 2, 2>>(&[2, 2], &[2, 3])
+            .pool_2d::<R5<1, 2, 1, 1, 1>>((2, 2), (2, 3))
+            .max_reduce::<_, LAxes2<2, 4>>();
+        out3.retrieve();
+
+        // Run all the computations
+        cx.execute();
+
+        println!(
+            "
+Expected Shape: {:?}
+Actual Shape: {:?}",
+            &[1, 2, 1],
+            out3.shape.dims
+        );
+        println!(
+            "
+Expected Tensor: {:?}
+Actual Tensor: {:?}",
+            &[20., 112.],
+            &out3.data()
+        );
+
+        println!(
+            "{:?}",
+            [12., 20., 30., 0., 8., 12., 2., 0., 34., 70., 37., 4., 112., 100., 25., 12.,]
+        );
+
+        assert_close(&out3.data(), &[20., 112.]);
     }
 
     #[test]
