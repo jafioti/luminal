7460fcde9da10448432fba195a842b511760c4a5
Hassan Hayat <hassan.hayat7@gmail.com>
2023-12-19 15:16:21 -0600

    Simple design, no pool_out

--- src/nn/convolution.rs
+++ src/nn/convolution.rs
@@ -1,11 +1,6 @@
 use crate::prelude::*;
 use rand::{thread_rng, Rng};
 
-// DIM_OUT = ((DIM_IN - (DILATION + 1) * (KERNEL - 1) - 1) / STRIDE) + 1
-
-// TODO: I think the math is more complex here with dilations
-// POOL_OUT = DIM_IN / STRIDE
-
 // Conv1D
 pub struct Conv1D<
     const CHANNELS_IN: usize,
@@ -13,6 +8,7 @@ pub struct Conv1D<
     const KERNEL: usize,
     const STRIDE: usize,
     const DILATION: usize,
+    const CHANNELS_IN_TIMES_KERNEL: usize,
 > {
     pub weight: GraphTensor<R3<CHANNELS_IN, CHANNELS_OUT, KERNEL>>,
 }
@@ -23,7 +19,9 @@ impl<
         const KERNEL: usize,
         const STRIDE: usize,
         const DILATION: usize,
-    > InitModule for Conv1D<CHANNELS_IN, CHANNELS_OUT, KERNEL, STRIDE, DILATION>
+        const CHANNELS_IN_TIMES_KERNEL: usize,
+    > InitModule
+    for Conv1D<CHANNELS_IN, CHANNELS_OUT, KERNEL, STRIDE, DILATION, CHANNELS_IN_TIMES_KERNEL>
 {
     fn initialize(cx: &mut Graph) -> Self {
         let conv = Self {
@@ -47,7 +45,9 @@ impl<
         const KERNEL: usize,
         const STRIDE: usize,
         const DILATION: usize,
-    > SerializeModule for Conv1D<CHANNELS_IN, CHANNELS_OUT, KERNEL, STRIDE, DILATION>
+        const CHANNELS_IN_TIMES_KERNEL: usize,
+    > SerializeModule
+    for Conv1D<CHANNELS_IN, CHANNELS_OUT, KERNEL, STRIDE, DILATION, CHANNELS_IN_TIMES_KERNEL>
 {
     fn serialize(&self, s: &mut crate::serialization::Serializer) {
         s.tensor("weight", self.weight);
@@ -61,24 +61,25 @@ impl<
         const KERNEL: usize,
         const STRIDE: usize,
         const DILATION: usize,
-    > Conv1D<CHANNELS_IN, CHANNELS_OUT, KERNEL, STRIDE, DILATION>
+        const CHANNELS_IN_TIMES_KERNEL: usize,
+    > Conv1D<CHANNELS_IN, CHANNELS_OUT, KERNEL, STRIDE, DILATION, CHANNELS_IN_TIMES_KERNEL>
 {
-    fn forward<const DIM_IN: usize, const DIM_OUT: usize, const POOL_OUT: usize>(
+    fn forward<const DIM_IN: usize, const DIM_OUT: usize>(
         &self,
         input: GraphTensor<R2<CHANNELS_IN, DIM_IN>>,
     ) -> GraphTensor<R2<CHANNELS_OUT, DIM_OUT>> {
         input
-            .pool_last_dim::<R3<CHANNELS_IN, POOL_OUT, KERNEL>>(
+            .pool_last_dim::<R3<CHANNELS_IN, DIM_OUT, KERNEL>>(
                 KERNEL.into(),
                 STRIDE.into(),
                 DILATION,
             )
             .permute::<_, Axes3<1, 0, 2>>()
-            .reshape::<R2<DIM_OUT, KERNEL>>()
+            .reshape::<R2<DIM_OUT, CHANNELS_IN_TIMES_KERNEL>>()
             .matmul(
                 self.weight
                     .permute::<_, Axes3<2, 1, 0>>()
-                    .reshape::<R2<KERNEL, CHANNELS_OUT>>(),
+                    .reshape::<R2<CHANNELS_IN_TIMES_KERNEL, CHANNELS_OUT>>(),
             )
             .permute::<_, Axes2<1, 0>>()
     }
@@ -96,20 +97,26 @@ mod tests {
         const CHANNELS_IN: usize = 1;
         const CHANNELS_OUT: usize = 1;
         const KERNEL: usize = 2;
-        const STRIDE: usize = KERNEL;
+        const STRIDE: usize = 2;
         const DILATION: usize = 0;
         const DIM_IN: usize = 6;
-        const DIM_OUT: usize = 3;
-        const POOL_OUT: usize = DIM_IN / STRIDE;
+        const DIM_OUT: usize = ((DIM_IN - (DILATION + 1) * (KERNEL - 1) - 1) / STRIDE) + 1;
+        const CHANNELS_IN_TIMES_KERNEL: usize = CHANNELS_IN * KERNEL;
 
-        let model: Conv1D<CHANNELS_IN, CHANNELS_OUT, KERNEL, KERNEL, DILATION> =
-            Conv1D::initialize(&mut cx);
+        let model: Conv1D<
+            CHANNELS_IN,
+            CHANNELS_OUT,
+            KERNEL,
+            KERNEL,
+            DILATION,
+            CHANNELS_IN_TIMES_KERNEL,
+        > = Conv1D::initialize(&mut cx);
         model.weight.set(vec![0.0316, -0.2057]);
 
         let inp1 = cx.tensor::<R2<1, 6>>();
         inp1.set(vec![3., 0., 9., 6., 0., 6.]);
 
-        let out1 = model.forward::<DIM_IN, DIM_OUT, POOL_OUT>(inp1).retrieve();
+        let out1 = model.forward::<DIM_IN, DIM_OUT>(inp1).retrieve();
         cx.execute();
 
         assert_close(&out1.data(), &[0.0948, -0.9498, -1.2342]);
