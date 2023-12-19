6f810111c4787348588b8fd955481171dae9868d
Joe Fioti <jafioti@gmail.com>
2023-12-19 17:20:57 -0500

    Tril and triu

--- src/hl_ops/other.rs
+++ src/hl_ops/other.rs
@@ -29,6 +29,22 @@ impl Graph {
     pub fn arange<N: Dimension>(&mut self) -> GraphTensor<(N,)> {
         self.constant(1.).expand().cumsum_last_dim() - 1.
     }
+
+    /// Lower left-hand triangle of 1s
+    pub fn tril<H: Dimension, W: Dimension>(&mut self, offset: i32) -> GraphTensor<(H, W)> {
+        let horizontal = self.arange::<W>().expand::<(H, W), _>();
+        let vertical = self.arange::<H>().expand::<(H, W), _>();
+
+        (horizontal + self.constant(-(offset as f32 + 1.)).expand()).less_than(vertical)
+    }
+
+    /// Lower left-hand triangle of 1s
+    pub fn triu<H: Dimension, W: Dimension>(&mut self, offset: i32) -> GraphTensor<(H, W)> {
+        let horizontal = self.arange::<W>().expand::<(H, W), _>();
+        let vertical = self.arange::<H>().expand::<(H, W), _>();
+
+        (horizontal + self.constant(-(offset as f32 - 1.)).expand()).greater_than(vertical)
+    }
 }
 
 #[cfg(test)]
@@ -43,6 +59,7 @@ mod tests {
 
         assert_exact(&arange.data(), &[0., 1., 2., 3., 4., 5., 6., 7., 8., 9.]);
     }
+
     #[test]
     fn test_dyn_arange() {
         let mut cx = Graph::new();
@@ -53,5 +70,61 @@ mod tests {
         cx.execute();
 
         assert_exact(&arange.data(), &[0., 1., 2., 3., 4., 5.]);
+    }
+
+    #[test]
+    fn test_tril() {
+        let mut cx = Graph::new();
+
+        let triangle = cx.tril::<LConst<5>, LConst<5>>(1).retrieve();
+
+        cx.execute();
+
+        assert_exact(
+            &triangle.data(),
+            &[
+                [1.00, 1.00, 0.00, 0.00, 0.00],
+                [1.00, 1.00, 1.00, 0.00, 0.00],
+                [1.00, 1.00, 1.00, 1.00, 0.00],
+                [1.00, 1.00, 1.00, 1.00, 1.00],
+                [1.00, 1.00, 1.00, 1.00, 1.00],
+            ]
+            .into_iter()
+            .flatten()
+            .collect::<Vec<_>>(),
+        );
+    }
+
+    #[test]
+    fn test_triu() {
+        let mut cx = Graph::new();
+
+        let a = cx.triu::<LConst<3>, LConst<3>>(-1).retrieve();
+        let b = cx.triu::<LConst<3>, LConst<3>>(0).retrieve();
+        let c = cx.triu::<LConst<3>, LConst<3>>(1).retrieve();
+
+        cx.execute();
+
+        assert_exact(
+            &a.data(),
+            &[[1.00, 1.00, 1.00], [1.00, 1.00, 1.00], [0.00, 1.00, 1.00]]
+                .into_iter()
+                .flatten()
+                .collect::<Vec<_>>(),
+        );
+        assert_exact(
+            &b.data(),
+            &[[1.00, 1.00, 1.00], [0.00, 1.00, 1.00], [0.00, 0.00, 1.00]]
+                .into_iter()
+                .flatten()
+                .collect::<Vec<_>>(),
+        );
+        assert_exact(
+            &c.data(),
+            &[[0.00, 1.00, 1.00], [0.00, 0.00, 1.00], [0.00, 0.00, 0.00]]
+                .into_iter()
+                .flatten()
+                .collect::<Vec<_>>(),
+        );
     }
 }
