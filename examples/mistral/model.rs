3a25325d375d04404faa326f8560d9785fd7cbf8
Hassan Hayat <hassan.hayat7@gmail.com>
2023-12-21 06:02:33 -0600

    Get tokenizer working

---
+++ examples/mistral/model.rs
@@ -0,0 +1,39 @@
+use itertools::Itertools;
+use luminal::prelude::*;
+use rust_tokenizers::{
+    error::TokenizerError,
+    tokenizer::{SentencePieceBpeTokenizer, Tokenizer, TruncationStrategy},
+};
+
+// Mistral 7B Config
+pub const VOCAB_SIZE: usize = 32000;
+pub const HIDDEN_DIM: usize = 4096;
+pub const LAYERS: usize = 32;
+pub const ATTENTION_PROJECTION_DIM: usize = 1024;
+pub const MLP_PROJECTION_DIM: usize = 14336;
+
+pub struct Mistral {
+    pub tokenizer: SentencePieceBpeTokenizer,
+    // pub next_token_graph: *mut Graph,
+    // pub prompt_processing_graph: *mut Graph,
+}
+
+impl Mistral {
+    pub fn new(tokenizer_path: &str) -> Result<Self, TokenizerError> {
+        let tokenizer = SentencePieceBpeTokenizer::from_file(tokenizer_path, false)?;
+
+        Ok(Self {
+            tokenizer: tokenizer,
+        })
+    }
+
+    // Method to encode text as vector
+    pub fn encode(self, text: &str) -> Vec<f32> {
+        self.tokenizer
+            .encode(text, None, text.len(), &TruncationStrategy::LongestFirst, 0)
+            .token_ids
+            .iter()
+            .map(|&x| x as f32)
+            .collect_vec()
+    }
+}
