268a9b2cf8a89e3bfd33d6d6d514df61945d4a06
Joe Fioti <jafioti@gmail.com>
2023-12-22 10:38:29 -0500

    Added metal equal compiler

--- src/compilers/metal/fp16/mod.rs
+++ src/compilers/metal/fp16/mod.rs
@@ -8,7 +8,7 @@ mod rms_norm;
 
 pub type MetalFp16Compiler = (
     super::prim::PrimitiveCompiler<f16>,
-    super::other::MetalSubtractionCompiler<f16>,
+    super::binary::MetalBinaryCompilers<f16>,
     // other::MetalCosCompiler, // For some reason doesn't produce the same outputs, need to test
     other::MetalExpCompiler,
     other::MetalGatherCompiler,
