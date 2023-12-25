6f3d52f3454e60fbf53965ae5ca96ed54787fd8f
Hassan Hayat <hassan.hayat7@gmail.com>
2023-12-25 12:52:57 -0600

    save progress

--- examples/mistral/main.rs
+++ examples/mistral/main.rs
@@ -8,66 +8,66 @@ use luminal::{
 };
 mod model;
 
-fn main() {
-    let mut graph = Graph::new();
+// fn main() {
+//     let mut graph = Graph::new();
 
-    let x = graph.arange_::<R2<4, 8>>();
-    x.retrieve();
+//     let x = graph.arange_::<R2<4, 8>>();
+//     x.retrieve();
 
-    // let y = x.slice((.., ..6));
-    // let y = x
-    //     .reshape::<R2<4, 6>>()
-    //     .pad::<R2<4, 8>, usize, usize>(&[(0, 0), (0, 2)]);
-    let mut y = x.contiguous();
-    y.shape.slice(&[(0.into(), 4.into()), (0.into(), 6.into())]);
-    y.retrieve();
+//     // let y = x.slice((.., ..6));
+//     // let y = x
+//     //     .reshape::<R2<4, 6>>()
+//     //     .pad::<R2<4, 8>, usize, usize>(&[(0, 0), (0, 2)]);
+//     let mut y = x.contiguous();
+//     y.shape.slice(&[(0.into(), 4.into()), (0.into(), 6.into())]);
+//     y.retrieve();
 
-    graph.compile(<(
-        PreGenericCompiler,
-        MetalFp16Compiler,
-        // CPUCompiler,
-        PostGenericCompiler,
-    )>::default());
+//     graph.compile(<(
+//         PreGenericCompiler,
+//         MetalFp16Compiler,
+//         // CPUCompiler,
+//         PostGenericCompiler,
+//     )>::default());
 
-    graph.execute();
+//     graph.execute();
 
-    println!("x: {:?}", x);
-    println!("y: {:?}", y);
-}
-
-// fn main() -> Result<(), String> {
-//     println!("Defining the model and loading the tokenizer");
-//     let mut mistral = Mistral::new("./examples/mistral/setup/mistral-7b-hf/tokenizer.model")
-//         .map_err(|e| e.to_string())?;
-
-//     let file_paths = [
-//         "./examples/mistral/setup/mistral-7b-hf/model-00001-of-00003.safetensors",
-//         "./examples/mistral/setup/mistral-7b-hf/model-00002-of-00003.safetensors",
-//         "./examples/mistral/setup/mistral-7b-hf/model-00003-of-00003.safetensors",
-//     ];
-
-//     println!("Loading the model weights from safetensors");
-//     unsafe {
-//         mistral
-//             .load_safe_tensors_from_files(file_paths.iter().map(|s| s.to_string()).collect_vec())?;
-//     }
-
-//     mistral.debug_run();
-
-//     // // Build the forward graph
-//     // println!("Building the forward graph");
-//     // let output_token_ids = mistral.build_forward_graph();
-
-//     // // Compile the graph
-//     // println!("Compiling the forward graph");
-//     // mistral.compile_forward_graph();
-
-//     // // Test inference
-//     // let input_text = "Hello, how are";
-//     // println!("Infering the next token");
-//     // let output_text = mistral.infer_next_token(output_token_ids, input_text);
-
-//     // println!("Inference: {output_text}");
-
-//     Ok(())
+//     println!("x: {:?}", x);
+//     println!("y: {:?}", y);
 // }
+
+fn main() -> Result<(), String> {
+    println!("Defining the model and loading the tokenizer");
+    let mut mistral = Mistral::new("./examples/mistral/setup/mistral-7b-hf/tokenizer.model")
+        .map_err(|e| e.to_string())?;
+
+    let file_paths = [
+        "./examples/mistral/setup/mistral-7b-hf/model-00001-of-00003.safetensors",
+        "./examples/mistral/setup/mistral-7b-hf/model-00002-of-00003.safetensors",
+        "./examples/mistral/setup/mistral-7b-hf/model-00003-of-00003.safetensors",
+    ];
+
+    println!("Loading the model weights from safetensors");
+    unsafe {
+        mistral
+            .load_safe_tensors_from_files(file_paths.iter().map(|s| s.to_string()).collect_vec())?;
+    }
+
+    mistral.debug_run();
+
+    // // Build the forward graph
+    // println!("Building the forward graph");
+    // let output_token_ids = mistral.build_forward_graph();
+
+    // // Compile the graph
+    // println!("Compiling the forward graph");
+    // mistral.compile_forward_graph();
+
+    // // Test inference
+    // let input_text = "Hello, how are";
+    // println!("Infering the next token");
+    // let output_text = mistral.infer_next_token(output_token_ids, input_text);
+
+    // println!("Inference: {output_text}");
+
+    Ok(())
+}
