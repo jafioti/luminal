eb5c40832ea7538c1d7d897b07c7a1248c1b2ea1
Hassan Hayat <hassan.hayat7@gmail.com>
2024-04-28 20:57:32 -0500

    Get model running, but not currently functioning

--- examples/llama_server/src/chat.rs
+++ examples/llama_server/src/chat.rs
@@ -75,16 +75,21 @@ pub async fn respond_chat_request(model: &mut Model, request: ChatRequest) -> Ch
     let raw_uuid = Uuid::new_v4();
     let id = format!("chatcmpl-{}", raw_uuid);
 
-    let prompt = apply_chat_template(request.messages);
+    let mut prompt = apply_chat_template(request.messages);
+    prompt += "<|start_header_id|>assistant<|end_header_id|>\n";
     let prompt_tokens = model.tokenizer.encode(prompt.clone(), false).unwrap();
     let prompt_tokens = prompt_tokens.get_ids();
     let prompt_tokens = prompt_tokens.len();
+    println!("Prompt: {:?}", prompt);
 
     // Generate
     let mut completion = vec![];
     model.generate(&prompt, |token| {
         completion.push(token);
-        true
+        println!(" {}", token);
+
+        // TODO: Remove this hack, it's a sign the generation is not working
+        token != 97720
     });
     let completion_str = model.tokenizer.decode(&completion, false).unwrap();
     let completion_tokens = completion.len();
--- examples/llama_server/src/main.rs
+++ examples/llama_server/src/main.rs
@@ -4,8 +4,8 @@ use axum::{
     routing::{get, post},
     Json, Router,
 };
-use std::sync::{Arc, Mutex, RwLock};
-use tokio::net::TcpListener;
+use std::sync::Arc;
+use tokio::{net::TcpListener, sync::Mutex};
 
 mod chat;
 mod llama;
@@ -37,30 +37,8 @@ async fn chat_completions(
     Extension(model): Extension<Arc<Mutex<Model>>>,
     Json(payload): Json<ChatRequest>,
 ) -> (StatusCode, Json<ChatResponse>) {
-    // let mut model = model.lock().unwrap(); // Acquire a write lock
+    let mut model = model.lock().await;
 
-    // let response = respond_chat_request(&mut *model, payload).await;
-    // (StatusCode::OK, Json(response))
-    (
-        StatusCode::OK,
-        Json(ChatResponse {
-            id: "".to_string(),
-            created: 1,
-            object: "chat.completion".to_string(),
-            model: "meta-llama/Meta-Llama-3-70B-Instruct".to_string(),
-            choices: vec![chat::Choice {
-                index: 0,
-                message: chat::Message {
-                    role: chat::Role::Assistant,
-                    content: "".to_string(),
-                },
-                finish_reason: "stop".to_string(),
-            }],
-            usage: chat::Usage {
-                total_tokens: 0,
-                prompt_tokens: 0,
-                completion_tokens: 0,
-            },
-        }),
-    )
+    let response = respond_chat_request(&mut *model, payload).await;
+    (StatusCode::OK, Json(response))
 }
