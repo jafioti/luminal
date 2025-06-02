5b1a1001de2847c89f70508d01d7805a468043c1
Joe Fioti <jafioti@gmail.com>
2025-06-02 15:48:43 -0500

    added co website

---
+++ docs/company/src/main.rs
@@ -0,0 +1,23 @@
+use axum::{
+    routing::get,
+    response::Html,
+    Router,
+};
+use std::{fs, net::SocketAddr};
+
+async fn index() -> Html<String> {
+    // Read the file at runtime so you can edit without rebuilding
+    let html = fs::read_to_string("static/index.html")
+        .expect("`static/index.html` not found");
+    Html(html)
+}
+
+#[tokio::main]
+async fn main() {
+    // single route for “/”
+    let app = Router::new().route("/", get(index));
+
+	let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+
+}
