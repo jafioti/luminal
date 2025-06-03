3ad169ed867f89b7451252f4c700a1a928307e9c
Joe Fioti <jafioti@gmail.com>
2025-06-03 10:54:31 -0700

    fmt

--- docs/company/src/main.rs
+++ docs/company/src/main.rs
@@ -1,14 +1,9 @@
-use axum::{
-    routing::get,
-    response::Html,
-    Router,
-};
-use std::{fs, net::SocketAddr};
+use axum::{Router, response::Html, routing::get};
+use std::fs;
 
 async fn index() -> Html<String> {
     // Read the file at runtime so you can edit without rebuilding
-    let html = fs::read_to_string("static/index.html")
-        .expect("`static/index.html` not found");
+    let html = fs::read_to_string("static/index.html").expect("`static/index.html` not found");
     Html(html)
 }
 
@@ -17,7 +12,6 @@ async fn main() {
     // single route for “/”
     let app = Router::new().route("/", get(index));
 
-	let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
-
 }
