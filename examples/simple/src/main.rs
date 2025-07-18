5aee7720920638a011f136e2f536f2e41f11a9f1
Joe Fioti <jafioti@gmail.com>
2025-07-18 14:14:02 -0700

    fixed simple

--- examples/simple/src/main.rs
+++ examples/simple/src/main.rs
@@ -2,10 +2,10 @@ use std::collections::HashMap;
 
 use itertools::Itertools;
 use luminal::prelude::{petgraph::Direction, *};
-use luminal_2::{
-    codegen::codegen, extract::search, run::run_graph, translate::translate_graph,
-    utils::build_search_space, GPUArch,
-};
+//use luminal_2::{
+// codegen::codegen, extract::search, run::run_graph, translate::translate_graph,
+// utils::build_search_space, GPUArch,
+//};
 use luminal_nn::Linear;
 use rand::{rng, Rng};
 
@@ -24,33 +24,33 @@ fn main() {
 
     // Execute the graph
     cx.execute_debug();
-    let (new_graph, accs) = translate_graph(&cx);
+    //let (new_graph, accs) = translate_graph(&cx);
     // luminal_2::utils::display_graph(&new_graph, &[]);
     // Print the results
-    println!("{:?}", accs);
+    //println!("{:?}", accs);
     println!("B: {:?}", b.data());
-    let root = new_graph.externals(Direction::Outgoing).next().unwrap();
-    let kernels = codegen(
-        new_graph.clone(),
-        root,
-        GPUArch::Metal(HashMap::default()),
-        0,
-    )
-    .unwrap();
-    let mut inputs = vec![
-        ("Tensor Load".to_string(), vec![1., 2., 3., 4.]),
-        ("Weight Load".to_string(), weight),
-    ];
-    for (label, val) in accs {
-        inputs.push((label, vec![val]));
-    }
+    // let root = new_graph.externals(Direction::Outgoing).next().unwrap();
+    // let kernels = codegen(
+    //     new_graph.clone(),
+    //     root,
+    //     GPUArch::Metal(HashMap::default()),
+    //     0,
+    // )
+    // .unwrap();
+    // let mut inputs = vec![
+    //     ("Tensor Load".to_string(), vec![1., 2., 3., 4.]),
+    //     ("Weight Load".to_string(), weight),
+    // ];
+    // for (label, val) in accs {
+    //     inputs.push((label, vec![val]));
+    // }
 
-    let outputs = run_graph(&inputs, &kernels);
-    println!("{:?}", outputs);
+    // let outputs = run_graph(&inputs, &kernels);
+    // println!("{:?}", outputs);
 
-    let egraph = build_search_space(&new_graph, 5, false);
-    let kernels = search(&egraph, &inputs, GPUArch::Metal(HashMap::default())).unwrap();
-    println!("kernels : {}", kernels.node_count() - 2);
-    let outputs = run_graph(&inputs, &kernels);
-    println!("{:?}", outputs);
+    // let egraph = build_search_space(&new_graph, 5, false);
+    // let kernels = search(&egraph, &inputs, GPUArch::Metal(HashMap::default())).unwrap();
+    // println!("kernels : {}", kernels.node_count() - 2);
+    // let outputs = run_graph(&inputs, &kernels);
+    // println!("{:?}", outputs);
 }
