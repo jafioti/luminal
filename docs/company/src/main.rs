57c266e78326a4e6327c7c235358d45c651ed23d
Joe Fioti <jafioti@gmail.com>
2025-07-27 21:25:24 -0700

    website update

--- docs/company/src/main.rs
+++ docs/company/src/main.rs
@@ -1,7 +1,6 @@
-use std::io::Bytes;
-
 use axum::{
     Router,
+    body::Bytes,
     http::header,
     response::{Html, IntoResponse, Redirect},
     routing::get,
@@ -20,14 +19,25 @@ async fn redirect() -> Redirect {
     Redirect::to("https://luminalai.com")
 }
 
+async fn favicon() -> impl IntoResponse {
+    // Embed at compile time
+    let ico = Bytes::from_static(include_bytes!("../../images/favicon.ico"));
+    (
+        [
+            (header::CONTENT_TYPE, "image/x-icon"),
+            (header::CACHE_CONTROL, "public, max-age=31536000, immutable"),
+        ],
+        ico,
+    )
+}
+
 #[tokio::main]
 async fn main() {
-    // single route for “/”
     let app = Router::new()
         .route("/", get(index))
         .route("/intro_quiz", get(intro_quiz))
         .route("/docs/introduction", get(redirect))
-        .route_service("/favicon.ico", ServeFile::new("../images/favicon.ico"));
+        .route_service("/favicon.ico", get(favicon));
 
     println!("Running on port 3000...");
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
